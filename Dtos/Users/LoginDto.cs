132911b8093d5f4b7f1d9eaade72af3663c37ac2
Lxzer0 <dmytro.korczynski@gmail.com>
2025-06-11 14:13:31 +0200

    fix: allow empty value in login request

--- Dtos/Users/LoginDto.cs
+++ Dtos/Users/LoginDto.cs
@@ -5,10 +5,8 @@ namespace CoursesManager.Dtos
 {
     public class LoginDto
     {
-        [Required]
         [JsonPropertyName("login")]
         public required string Email { get; set; }
-        [Required]
         [JsonPropertyName("pass")]
         public required string Password { get; set; }
     }
